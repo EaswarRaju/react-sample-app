29281407712b14a4aea4f37ef979684d018c4a10
Easwar Raju <easwarraju.it@gmail.com>
2020-02-11 13:56:55 +1100

    commit updated build

---
+++ precache-manifest.c92404dfc5a7290f2e58e15663ccf41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d56992df09fdd83af993329b9ab005a",
+    "url": "/react-sample-app/index.html"
+  },
+  {
+    "revision": "51e80b758d665e2f039b",
+    "url": "/react-sample-app/static/css/main.6e69eb3b.chunk.css"
+  },
+  {
+    "revision": "ab4473729b2170717790",
+    "url": "/react-sample-app/static/js/2.827f726f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-sample-app/static/js/2.827f726f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51e80b758d665e2f039b",
+    "url": "/react-sample-app/static/js/main.99568b38.chunk.js"
+  },
+  {
+    "revision": "e1ea84bd1888bb131d62",
+    "url": "/react-sample-app/static/js/runtime-main.a5d99a81.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2eee80b5ee5c798246865241f101cdc4.js"
+  "/react-sample-app/precache-manifest.c92404dfc5a7290f2e58e15663ccf41c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sample-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
